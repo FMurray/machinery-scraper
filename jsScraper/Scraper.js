8563d443a8a98b8c74c900fd4807a3ae858cf16d
Dnapert <dannapert@gmail.com>
2023-01-19 14:21:17 -0500

    trying everything

--- jsScraper/Scraper.js
+++ jsScraper/Scraper.js
@@ -1,28 +1,60 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
-let scrapedData = [];
-(async()=>{
+//let scrapedData = [];
+/* (async()=>{
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    const fakeUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
     try{
-        await page.goto('http://i-know-you-faked-user-agent.glitch.me/',{waitUntil: 'networkidle2'}).then(
+        
+         await page.evaluateOnNewDocument(() => {
+            let open = window.open;
+            window.open = (...args) => {
+                console.log(...args)
+                let newPage = open(...args);
+                Object.defineProperty(newPage.navigator, 'userAgent',{ get: ()=> fakeUserAgent});
+                return newPage;
+            }
+            window.open.toString=()=> 'function open() { [native code] }';
+        },fakeUserAgent); 
+        await page.setUserAgent(fakeUserAgent);
+        await page.goto('http://www.i-know-you-faked-user-agent.glitch.me/new-window',{waitUntil: 'domcontentloaded'}).then(
             console.log("Page loaded")
         )
-        await page.evaluateOnNewDocument(() => {
-            Object.defineProperty(navigator,'platform',{get:()=>'Linux x86_64'})
-            Object.defineProperty(navigator,'productSub',{get:()=>'20030107'})
-            Object.defineProperty(navigator,'vendor',{get:()=>'Google Inc.'})
-            Object.defineProperty(navigator,'oscpu',{get:()=>undefined})
-            Object.defineProperty(navigator,'cpuClass',{get:()=>undefined})
-        });
-        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36');
-        /*  const body = await page.evaluate(() => 
+          const body = await page.evaluate(() => 
             document.body.innerHTML); 
             let $ = cheerio.load(body);
             let title = $(".listing-portion-title").text();
             let currentBid = $(".auction-price").text();
-            scrapedData.push({"title":title, "currentBid":currentBid}); */
+            scrapedData.push({"title":title, "currentBid":currentBid}); 
+        }
+
+    catch(err){
+        console.log(err);
+    }
+    //await browser.close();
+console.log(scrapedData)}
+    )(); */
+
+
+let scrapedData = [];
+(async()=>{
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    const fakeUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+    try{
+       
+        await page.setUserAgent(fakeUserAgent);
+        await page.goto('http://i-know-you-faked-user-agent.glitch.me/',{waitUntil: 'domcontentloaded'}).then(
+            console.log("Page loaded")
+        )
+        const body = await page.evaluate(() => 
+            document.body.innerHTML); 
+            let $ = cheerio.load(body);
+            let title = $(".listing-portion-title").text();
+            let currentBid = $(".auction-price").text();
+            scrapedData.push({"title":title, "currentBid":currentBid}); 
         }
 
     catch(err){
@@ -30,4 +62,4 @@ let scrapedData = [];
     }
     await browser.close();
 console.log(scrapedData)}
-    )();
+)();
