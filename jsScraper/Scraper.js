1aaa2cae835cc2841d01134e702459fa38619435
Dnapert <dannapert@gmail.com>
2023-01-19 20:18:02 -0500

    testing on machine tools

--- jsScraper/Scraper.js
+++ jsScraper/Scraper.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const userAgentUrl= 'http://i-know-you-faked-user-agent.glitch.me/';
 const bookScraperUrl ='https://books.toscrape.com'
-
+const latheUrl = 'https://www.machinetools.com/en/for-sale/machines/used/swiss-type-automatic-screw-machines'
 
 let scrapedData = [];
 (async()=>{
@@ -10,15 +10,17 @@ let scrapedData = [];
     const page = await browser.newPage();
     const fakeUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
     await page.setUserAgent(fakeUserAgent);//set user agent
-    await page.goto(bookScraperUrl,{waitUntil: 'domcontentloaded'})//wait until domcontentloaded
+    await page.goto(latheUrl,{waitUntil: 'domcontentloaded'})//wait until dom content loaded
     const body = await page.evaluate(() => //evaluate is a puppeteer method that allows you to execute javascript in the browser
         document.body.innerHTML);
     let $ = cheerio.load(body);//load body into cheerio
-    let titles = $("h3"); ///cheerio method to select elements
+    let titles = $(".btr-support-link");
+   
     titles.each((index,element) => { ///cheerio method to iterate over a collection of elements
       let title = $(element).text();    //get innerText of element
         scrapedData.push({"title":title});//push to array
     });
+   
     let nextButton = $("li.next > a");
     if(nextButton.length > 0){
         /* 
